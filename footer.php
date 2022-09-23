3f1446fb03fe87f1486d2b1093b17c9eefd2bc70
chamberezigbo <chamberezigbo@gmail.com>
2022-09-23 18:28:01 +0100

    added another chating system

--- footer.php
+++ footer.php
@@ -25,21 +25,44 @@
          </div>
     </footer>
 
-    <!--Start of Tawk.to Script-->
-     <script type="text/javascript">
-     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-     (function(){
-     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-     s1.async=true;
-     s1.src='https://embed.tawk.to/62c03fa67b967b117997abcf/1g6vfh9i7';
-     s1.charset='UTF-8';
-     s1.setAttribute('crossorigin','*');
-     s0.parentNode.insertBefore(s1,s0);
-     })();
-     </script>
-<!--End of Tawk.to Script-->
-    <!--End of Tawk.to Script-->
-    <!--End of Tawk.to Script-->
+    <!-- Start of LiveChat (www.livechat.com) code -->
+    <script>
+         window.__lc = window.__lc || {};
+         window.__lc.license = 14552994;;
+         (function(n, t, c) {
+              function i(n) {
+                   return e._h ? e._h.apply(null, n) : e._q.push(n)
+              }
+              var e = {
+                   _q: [],
+                   _h: null,
+                   _v: "2.0",
+                   on: function() {
+                        i(["on", c.call(arguments)])
+                   },
+                   once: function() {
+                        i(["once", c.call(arguments)])
+                   },
+                   off: function() {
+                        i(["off", c.call(arguments)])
+                   },
+                   get: function() {
+                        if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+                        return i(["get", c.call(arguments)])
+                   },
+                   call: function() {
+                        i(["call", c.call(arguments)])
+                   },
+                   init: function() {
+                        var n = t.createElement("script");
+                        n.async = !0, n.type = "text/javascript", n.src = "https://cdn.livechatinc.com/tracking.js", t.head.appendChild(n)
+                   }
+              };
+              !n.__lc.asyncInit && e.init(), n.LiveChatWidget = n.LiveChatWidget || e
+         }(window, document, [].slice))
+    </script>
+    <noscript><a href="https://www.livechat.com/chat-with/14552994/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a></noscript>
+    <!-- End of LiveChat code -->
 
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -51,7 +74,7 @@
     <script src="res/js/form.validation.js"></script>
     <script src="res/js/activation.validation.js"></script>
     <script src="res/js//loginForm.validation.js"></script>
-        <script src="res/js/myscrept.js"></script>
+    <script src="res/js/myscrept.js"></script>
     <script>
          setInterval(function() {
               let alertDev = document.getElementById('alertActivation').classList.add("d-none")
